2307223e31bf5c4062bd89aaa02f0eff620241a8
Damien <damien.deboudt@sfr.fr>
2022-12-20 10:41:00 +0100

    JS ou jquery

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const tom = new Audio('sounds/tom.wav')
 
 
 
+/*
 document.addEventListener("keypress",function (p){
     if(p.key === "g") {
         boom.play()
@@ -36,6 +37,45 @@ document.addEventListener("keypress",function (p){
         tom.play
     }
     if (p.key ==='l') {
+        tink.play()
+    }
+})*/
+
+$('body').keydown(function (event) {
+  if(event.wich === 65) {
+      $('.key(data-key = 65)').addClass('playing');
+      clap.play()
+  }
+    if(event.wich === 83) {
+        $('.key(data-key = 83)').addClass('playing');
+        hihat.play()
+    }
+    if(event.wich === 68) {
+        $('.key(data-key = 68)').addClass('playing');
+        kick.play()
+    }
+    if(event.wich === 70) {
+        $('.key(data-key = 70)').addClass('playing');
+        openhat.play()
+    }
+    if(event.wich === 71) {
+        $('.key(data-key = 71)').addClass('playing');
+        boom.play()
+    }
+    if(event.wich === 72) {
+        $('.key(data-key = 72)').addClass('playing');
+        ride.play()
+    }
+    if(event.wich === 74) {
+        $('.key(data-key = 74)').addClass('playing');
+        snare.play()
+    }
+    if(event.wich === 75) {
+        $('.key(data-key = 75)').addClass('playing');
+        tom.play()
+    }
+    if(event.wich === 76) {
+        $('.key(data-key = 76)').addClass('playing');
         tink.play()
     }
 })
