e04aa9131262cb318c05701df9c2c8a1832c73ab
Damien <damien.deboudt@sfr.fr>
2022-12-21 10:51:54 +0100

    terminé

--- script.js
+++ script.js
@@ -9,73 +9,41 @@ const tink = new Audio('sounds/tink.wav')
 const tom = new Audio('sounds/tom.wav')
 
 
-
-/*
-document.addEventListener("keypress",function (p){
-    if(p.key === "g") {
-        boom.play()
-    }
-    if(p.key=== "a" ){
-        clap.play()
-    }
-    if(p.key === "s"){
-        hihat.play()
-    }
-    if(p.key === "d") {
-        kick.play
-    }
-    if(p.key === "f") {
-        openhat.play()
-    }
-    if(p.key === "h") {
-        ride.play()
-    }
-    if (p.key === "j") {
-        snare.play()
-    }
-    if(p.key === 'k') {
-        tom.play
-    }
-    if (p.key ==='l') {
-        tink.play()
-    }
-})*/
-
-$('body').keydown(function (event) {
-  if(event.wich === 65) {
-      $('.key(data-key = 65)').addClass('playing');
+$('body').keydown(function (event){
+  if(event.which === 65) {
+      $('.key[data-key = 65]').addClass('playing');
       clap.play()
   }
-    if(event.wich === 83) {
-        $('.key(data-key = 83)').addClass('playing');
+    if(event.which === 83) {
+        $('.key[data-key = 83]').addClass('playing');
         hihat.play()
     }
-    if(event.wich === 68) {
-        $('.key(data-key = 68)').addClass('playing');
+    if(event.which === 68) {
+        $('.key[data-key = 68]').addClass('playing');
         kick.play()
     }
-    if(event.wich === 70) {
-        $('.key(data-key = 70)').addClass('playing');
+    if(event.which === 70) {
+        $('.key[data-key = 70]').addClass('playing');
         openhat.play()
     }
-    if(event.wich === 71) {
-        $('.key(data-key = 71)').addClass('playing');
+    if(event.which === 71) {
+        $('.key[data-key = 71]').addClass('playing');
         boom.play()
     }
-    if(event.wich === 72) {
-        $('.key(data-key = 72)').addClass('playing');
+    if(event.which === 72) {
+        $('.key[data-key = 72]').addClass('playing');
         ride.play()
     }
-    if(event.wich === 74) {
-        $('.key(data-key = 74)').addClass('playing');
+    if(event.which === 74) {
+        $('.key(data-key = 74]').addClass('playing');
         snare.play()
     }
-    if(event.wich === 75) {
-        $('.key(data-key = 75)').addClass('playing');
+    if(event.which === 75) {
+        $('.key[data-key = 75]').addClass('playing');
         tom.play()
     }
-    if(event.wich === 76) {
-        $('.key(data-key = 76)').addClass('playing');
+    if(event.which === 76) {
+        $('.key[data-key = 76]').addClass('playing');
         tink.play()
     }
 })
